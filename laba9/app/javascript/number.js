60b6507d294b9e46821e5ca14db60d9a8317b485
p134d <p134d@mail.ru>
2020-11-22 23:53:47 +0300

    laba9

---
+++ laba9/app/javascript/number.js
@@ -0,0 +1,14 @@
+
+function show_result(data) {
+    let result = document.getElementById("result");
+    result.innerHTML = "<hr/>Result is: " + data.value + "<hr/>";
+}
+
+$(document).ready(function() {
+    $("#number_form").on("ajax:success", function(event) {
+        [data, status, xhr] = event.detail
+        show_result(data)
+    }).on("ajax:error", function(event) {
+        $("#result").html("<p>Ошибка при AJAX-запросе</p>")
+    })
+})
