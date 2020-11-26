0d94211e53cfc4d6989f4eebfc0c648680fed32b
p134d <p134d@mail.ru>
2020-11-26 19:44:23 +0300

    selenium tests for laba9

--- laba9/app/javascript/packs/number.js
+++ laba9/app/javascript/packs/number.js
@@ -1,15 +1,30 @@
 
 function show_result(data) {
     let result = document.getElementById("result");
-    result.innerHTML = "<hr/>Result is: " + data.value + "<hr/>";
+    if (data.type == "Array") {
+        let text = "<table border=\"1\">   <caption>Результат</caption>";
+        data.value.forEach(function (item, i, arr) {
+            text += "<tr>" + "<td>" + item[0] + "</td>" + "<td>" + item[1] + "</td>" + "</tr>"
+        });
+        text += "</tabel>"
+        result.innerHTML = text;
+    } else {
+        if (data.value==-1){
+            result.innerHTML = "Некорректные данные";
+        } else{
+            result.innerHTML = "Дружественных чисел нет";
+        }
+    }
+
 }
 
-$(document).ready(function() {
-    $("#number_form").on("ajax:success", function(event) {
-        [data, status, xhr] = event.detail
+window.addEventListener("load", () => {
+    const element = document.querySelector("#number_form");
+    element.addEventListener("ajax:success", (event) => {
+        [data, status, xhr] = event.detail;
         show_result(data)
-    }).on("ajax:error", function(event) {
+    });
+    element.addEventListener("ajax:error", () => {
         $("#result").html("<p>Ошибка при AJAX-запросе</p>")
-    })
-})
-
+    });
+});
